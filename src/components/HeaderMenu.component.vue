8bf22410e09b25aae0dea574ae438a193b7f9903
Enzo <fournierenzo62@gmail.com>
2025-06-13 16:42:03 +0200

    feat(route)

---
+++ src/components/HeaderMenu.component.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-menu :options="menuOptions" mode="horizontal" />
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const menuOptions = [
+  {
+    label: 'Home',
+    key: '/',
+    onClick: () => router.push('/')
+  }
+];
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <template>
   <main>
-    <HeaderMenuComponent/>
     <p>hello world</p>
   </main>
 </template>
 
 <script setup lang="ts">
-import HeaderMenuComponent from '../components/HeaderMenu.component.vue';
 </script>
